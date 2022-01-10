0df8891ed701ce79b2e6e8f30fb9fcaf37899967
josan <josanescod@gmail.com>
2022-01-10 09:28:35 +0100

    Header and Footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,16 @@
 <script>
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
 </script>
 
 <main>
-  <h1>Hello world!</h1>
+  <Header />
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero in excepturi
+    cumque ut laborum. Iure cumque harum eos aliquam, consequuntur veritatis!
+    Velit commodi ratione quis mollitia facere! Rerum, placeat quod.
+  </p>
+  <Footer />
 </main>
 
 <style>
---
+++ src/components/Footer.svelte
@@ -0,0 +1,21 @@
+<script>
+  const date = new Date().getFullYear();
+</script>
+
+<footer>
+  <div class="copyright">{date}</div>
+</footer>
+
+<style>
+  footer {
+    padding: 40px;
+    text-align: center;
+  }
+  .copyright {
+    color: #aaa;
+    font-size: 14px;
+    display: inline-block;
+    padding: 20px;
+    border-top: 1px solid #ddd;
+  }
+</style>
