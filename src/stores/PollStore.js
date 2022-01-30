78c6807ea5bc8c1cd37bd4c49928294ad6af4587
josan <josanescod@gmail.com>
2022-01-30 09:01:18 +0100

    new polls

--- src/stores/PollStore.js
+++ src/stores/PollStore.js
@@ -3,10 +3,18 @@ import { writable } from "svelte/store";
 const PollStore = writable([
   {
     id: 1,
-    question: "Python or JavaScript?",
-    answerA: "Python",
-    answerB: "JavaScript",
-    votesA: 9,
+    question: "Linux or BSD?",
+    answerA: "Linux",
+    answerB: "BSD",
+    votesA: 15,
+    votesB: 10,
+  },
+  {
+    id: 2,
+    question: "Rust or Go?",
+    answerA: "Rust",
+    answerB: "Go",
+    votesA: 12,
     votesB: 15,
   },
 ]); //this create data store
